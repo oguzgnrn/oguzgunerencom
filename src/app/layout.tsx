39cb66416eb3d815d56ee9553ee3055ef55a65b8
Oğuz Güneren <96068121+oguzgnrn@users.noreply.github.com>
2025-08-08 12:46:51 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import type { Metadata } from 'next'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
